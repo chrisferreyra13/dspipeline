f2aa0a9f90488191ed55da0d3adb6117ee45fee0
Manuel Alfonso <manuel.alfonso@neoris.com>
2021-04-19 00:04:37 -0300

    resize_image and save_image

--- process_images_example.py
+++ process_images_example.py
@@ -3,10 +3,8 @@ import argparse
 from processor import Processor
 
 from transformers.load_images import LoadImages
-from transformers.detect_faces import DetectFaces
-from transformers.save_faces import SaveFaces
-from transformers.save_summary import SaveSummary
-from transformers.display_summary import DisplaySummary
+from transformers.resize_image import ResizeImage
+from transformers.save_image import SaveImage
 
 
 def parse_args():
@@ -19,12 +17,6 @@ def parse_args():
                     help="path to output directory")
     ap.add_argument("-os", "--out-summary", default="summary.json",
                     help="output JSON summary file name")
-    ap.add_argument("--prototxt", default="./models/face_detector/deploy.prototxt.txt",
-                    help="path to Caffe 'deploy' prototxt file")
-    ap.add_argument("--model", default="./models/face_detector/res10_300x300_ssd_iter_140000.caffemodel",
-                    help="path to Caffe pre-trained model")
-    ap.add_argument("--confidence", type=float,  default=0.5,
-                    help="minimum probability to filter weak face detections")
     ap.add_argument("--batch-size", type=int, default=1,
                     help="face detection batch size")
 
@@ -35,23 +27,15 @@ def main(args):
     # Create pipeline steps
 
     load_images = LoadImages(args.input)
-
-    detect_faces = DetectFaces(prototxt=args.prototxt, model=args.model,
-                               confidence=args.confidence, batch_size=args.batch_size)
-
-    save_faces = SaveFaces(args.output)
-
-    summary_file = os.path.join(args.output, args.out_summary)
-    save_summary = SaveSummary(summary_file)
-
-    display_summary = DisplaySummary()
+    resize_image = ResizeImage(width=28, height=28)
+    save_image = SaveImage(args.output)
 
     # Create image processing pipeline
-    pipeline = (load_images |
-                detect_faces |
-                save_faces |
-                save_summary |
-                display_summary)
+    pipeline = (
+        load_images |
+        resize_image |
+        save_image
+    )
     
     # Create processor for processing pipeline
     process=Processor(pipeline)
@@ -60,8 +44,7 @@ def main(args):
     except:
         return
     finally:
-            print(f"[INFO] Saving summary to {summary_file}...")
-            save_summary.write()
+            print(f"[INFO] Finalizing process [{process.id}]...")
 
 if __name__ == "__main__":
     #args = parse_args()    # Disable during debugging 
@@ -69,9 +52,7 @@ if __name__ == "__main__":
         input="assets/images", 
         output="output",
         out_summary="output.json",
-        prototxt="./models/face_detector/deploy.prototxt.txt",
-        model="./models/face_detector/res10_300x300_ssd_iter_140000.caffemodel",
-        confidence=0.5,
-        batch_size=1)   # Disable when run through terminal
+        batch_size=1
+        )   # Disable when run through terminal
 
     main(args)
--- processor.py
+++ processor.py
@@ -1,9 +1,11 @@
+import random
 
 
 class Processor():
     """Handle processes and diplay summaries."""
     def __init__(self, process):
         self.process=process
+        self.id=random.randrange(10**2,10**4)
 
     def run(self, verbose=True):
         try:
